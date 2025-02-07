8494dcdcbf6d8f9114d52d8b321bc490f51afed4
manstoi <manuel.stoilov.bg@gmail.com>
2025-02-07 15:21:37 -0500

    Set up static site

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('Welcome to FindMyPeoples!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {

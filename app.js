3a73dfb175b4683059fcdad2851ca91be78352d7
manstoi <manuel.stoilov.bg@gmail.com>
2025-02-07 14:40:07 -0500

    Initial commit

--- app.js
+++ app.js
@@ -3,26 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-    });
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    });
-
-const groups = [
-    { name: 'Book Club', description: 'A group for book lovers.' },
-    { name: 'Fitness Group', description: 'A group for fitness enthusiasts.' },
-];
-
-app.get('/groups', (req, res) => {
-    res.json(groups);
+  res.send('Welcome to FindMyPeoples!');
 });
 
-app.use(express.json());
-
-app.post('/suggest-group', (req, res) => {
-    const newGroup = req.body;
-    groups.push(newGroup);
-    res.status(201).json(newGroup);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
